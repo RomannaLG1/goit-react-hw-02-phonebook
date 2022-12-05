aa70b858583589591d56ba30396273cb0e9e3074
RomannaLG1 <melnykromasja@gmail.com>
2022-12-05 12:46:35 +0200

    add all

---
+++ src/components/ContactListItem/ContactListItem.js
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+import { ListItemStyled, DeleteButton, ItemText, ItemLink } from './ContactListItem.styled';
+import {TiDelete} from 'react-icons/ti';
+
+export const ContactListItem = ({ name, number, onDeleteContact }) => (
+  <ListItemStyled>
+    <ItemLink>
+      <ItemText>{name}</ItemText>
+    <ItemText>{number}</ItemText>
+    </ItemLink>
+    
+    <DeleteButton type="button" onClick={onDeleteContact}> <TiDelete size="20"/> </DeleteButton>
+  </ListItemStyled>
+);
+
+ContactListItem.propTypes = {
+    name: PropTypes.string.isRequired,
+number: PropTypes.string.isRequired,
+onDeleteContact: PropTypes.func.isRequired,
+}
